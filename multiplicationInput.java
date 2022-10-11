c3f0fa7f46fd370b59844ac51de8fb3b22d8895e
BiancaTate <tatebianca1204@gmail.com>
2022-10-11 18:41:16 -0400

    multiplication input

---
+++ multiplicationInput.java
@@ -0,0 +1,28 @@
+package javaVariables;
+
+import java.util.*;
+
+
+public class multiplicationInput {
+	public static void main (String [] args) {
+		Scanner input= new Scanner (System.in);
+		System.out.print("Enter a number:  ");
+		int number = input.nextInt();
+			for( int i=1;i<=10;i++) {
+				System.out.println(number + " x " + i + "=" + number*i);
+			}
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		input.close();
+	
+}
+	
+
+}
